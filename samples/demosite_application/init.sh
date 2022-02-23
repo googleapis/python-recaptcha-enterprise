c908b7a0c87533feefdf10bbfb1c4a890e2707fb
Sita Lakshmi Sangameswaran <sita1996@gmail.com>
2022-02-23 20:16:43 +0530

    updated init.sh to allow all domains

--- samples/demosite_application/init.sh
+++ samples/demosite_application/init.sh
@@ -7,11 +7,11 @@ gcloud services enable recaptchaenterprise.googleapis.com
 export GOOGLE_CLOUD_PROJECT=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
 
 # gcloud command to create reCAPTCHA keys.
-sudo gcloud alpha recaptcha keys create --display-name=demo-recaptcha-score-key --web --domains=['cloudshell.dev'] --integration-type=SCORE 1>/dev/null 2>recaptchascorekeyfile
+gcloud alpha recaptcha keys create --display-name=demo-recaptcha-score-key --web --allow-all-domains --integration-type=SCORE 1>/dev/null 2>recaptchascorekeyfile
 export SITE_KEY=$(cat recaptchascorekeyfile | sed -n -e 's/.*Created \[\([0-9a-zA-Z_-]\+\)\].*/\1/p')
-sudo gcloud alpha recaptcha keys create --display-name=demo-recaptcha-checkbox-key --web --domains=['cloudshell.dev'] --integration-type=CHECKBOX 1>/dev/null 2>recaptchacheckboxkeyfile
+gcloud alpha recaptcha keys create --display-name=demo-recaptcha-checkbox-key --web --allow-all-domains --integration-type=CHECKBOX 1>/dev/null 2>recaptchacheckboxkeyfile
 export CHECKBOX_SITE_KEY=$(cat recaptchacheckboxkeyfile | sed -n -e 's/.*Created \[\([0-9a-zA-Z_-]\+\)\].*/\1/p')
 
 # build and run the local dockerfile in the port 8080.
-sudo docker build --build-arg GOOGLE_CLOUD_PROJECT=GOOGLE_CLOUD_PROJECT --build-arg SITE_KEY=SITE_KEY --build-arg CHECKBOX_SITE_KEY=CHECKBOX_SITE_KEY -t test .
-sudo docker run -d -p 8080:8080 test:latest
+docker build --build-arg GOOGLE_CLOUD_PROJECT=GOOGLE_CLOUD_PROJECT --build-arg SITE_KEY=SITE_KEY --build-arg CHECKBOX_SITE_KEY=CHECKBOX_SITE_KEY -t test .
+docker run -d -p 8080:8080 test:latest
